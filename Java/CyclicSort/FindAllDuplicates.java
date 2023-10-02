3a895d44042aff10d9697d0c861f75f4641fe406
PriyaP2002 <113006566+PriyaP2002@users.noreply.github.com>
2023-10-02 16:40:51 +0530

    Create FindAllDuplicates.java

---
+++ Java/CyclicSort/FindAllDuplicates.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+public class FindAllduplicates{
+    public static void main(String[] args) {
+        
+        int[] arr={4,3,2,7,8,2,3,1};
+        int h[]= finddup(arr);
+        System.out.println(Arrays.toString(h));
+
+    }
+
+    private static int[] finddup(int[] arr) {
+
+        List<Integer> list= new ArrayList<>();
+        int i=0;
+        while(i<arr.length){
+            int c=arr[i]-1;
+            if(arr[i]!=arr[c]){
+                swap(arr,c,i);
+            }
+            else{
+                i++;
+            }
+        }
+    
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=j+1){
+                list.add(arr[j]);
+            }
+        }
+        int[] b= new int[list.size()];
+        int m=0;
+        for(int k:list){
+            b[m]=k;m++;
+        }
+        return b;
+    }
+
+    private static void swap(int[] arr, int c, int i) {
+        int t=arr[i];
+        arr[i]=arr[c];
+        arr[c]=t;
+    }
+
+}
